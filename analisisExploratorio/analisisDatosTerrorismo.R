c898618f617430271e95d36331a6f6e78cb7022f
Rodriginsky <rodrigoestrategia2000@gmail.com>
2023-11-25 21:28:55 -0600

    First approach to cuantitative columns     mod analisisExploratorio/analisisDatosTerrorismo.R         For getting characteristics of cualitative attributes by dyplr summary and skimr aided analysis

--- analisisExploratorio/analisisDatosTerrorismo.R
+++ analisisExploratorio/analisisDatosTerrorismo.R
@@ -1,2 +1,12 @@
-data = read.csv(file = "RUTA/globalterrorismdb_0718dist.csv", sep=",", header = T, stringsAsFactors = F)
+data = read.csv(file = "E:/Descargas/Repos/AMD-2024-1/globalterrorismdb_0718dist.csv", sep=",", header = T, stringsAsFactors = F)
+select(data)
+library(dplyr)
+
+rod <- select(data,nwoundus, nwoundte, property, propextent, propextent_txt, propvalue, propcomment, ishostkid, nhostkid, nhostkidus, nhours, ndays, divert, kidhijcountry, ransom, ransomamt, ransomamtus, ransompaid, ransompaidus, ransomnote, hostkidoutcome, hostkidoutcome_txt, nreleased, addnotes, scite1, scite2, scite3, dbsource, INT_LOG, INT_IDEO, INT_MISC, INT_ANY, related
+)
+summary(rod)
+
+library(skimr)
+skim(rod)
+
 
