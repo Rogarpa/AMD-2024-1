a1b052c8bea9440964d296f44ca1fd9889c3fcad
Carlos Andrade Hernández <carlosandrade@ciencias.unam.mx>
2023-12-03 15:32:52 -0600

    Update prepDatosTerrorismo.R

--- preprocesamientoDeDatos/prepDatosTerrorismo.R
+++ preprocesamientoDeDatos/prepDatosTerrorismo.R
@@ -1,15 +1,17 @@
 install.packages("caret")
 
+
+
 library(dplyr)
 library(caret)
 
-data = read.csv(file = "RUTA/globalterrorismdb_0718dist.csv", sep=",", header = T, stringsAsFactors = F)
+data <- read.csv("/home/carlows/Descargas/globalterrorismdb_0718dist.csv")
 
 # Seleccionar atributos relevantes
 selected_data <- data %>% select(individual, nkill, nwound, weaptype1_txt, attacktype1_txt, targtype1_txt, gname, country_txt)
 
 # Manejo de valores perdidos
-# Para numéricos (nkill, nwound), usar la mediana para la imputacion
+# Para numericos (nkill, nwound), usar la mediana para la imputacion
 selected_data <- selected_data %>% mutate(nkill = ifelse(is.na(nkill), median(nkill, na.rm = TRUE), nkill))
 selected_data <- selected_data %>% mutate(nwound = ifelse(is.na(nwound), median(nwound, na.rm = TRUE), nwound))
 
@@ -25,18 +27,37 @@ selected_data$targtype1_txt <- ifelse(is.na(selected_data$targtype1_txt), impute
 selected_data$gname <- ifelse(is.na(selected_data$gname), impute_mode(selected_data$gname), selected_data$gname)
 selected_data$country_txt <- ifelse(is.na(selected_data$country_txt), impute_mode(selected_data$country_txt), selected_data$country_txt)
 
-# Eliminacion de valores atípicos
-bounds <- calculate_bounds(selected_data$individual)
-selected_data <- selected_data %>% filter(individual >= bounds$lower & individual <= bounds$upper)
+# Definir una funcion para calcular los limites para identificar valores atipicos
+calculate_bounds <- function(x) {
+  Q1 <- quantile(x, 0.25, na.rm = TRUE)
+  Q3 <- quantile(x, 0.75, na.rm = TRUE)
+  IQR <- Q3 - Q1
+  lower_bound <- Q1 - 1.5 * IQR
+  upper_bound <- Q3 + 1.5 * IQR
+  return(c(lower = lower_bound, upper = upper_bound))
+}
 
-# Discretización nkill
+# Eliminacion de valores atipicos
+# Definir función para reemplazar valores atípicos con la mediana
+replace_outliers <- function(x) {
+  bounds <- calculate_bounds(x)
+  x[x < bounds["lower"] | x > bounds["upper"]] <- median(x, na.rm = TRUE)
+  return(x)
+}
+
+# Aplicamos la eliminacion de valores atipicos por la media
+selected_data$nkill <- replace_outliers(selected_data$nkill)
+selected_data$nwound <- replace_outliers(selected_data$nwound)
+selected_data$individual <- replace_outliers(selected_data$individual)
+
+# Discretizacion nkill
 selected_data$nkill_discretizado <- cut(selected_data$nkill, breaks=c(0, 10, 50, max(selected_data$nkill, na.rm = TRUE)), labels=c("Bajo", "Medio", "Alto"))
 
-# Normalización de variables numéricas (nkill, nwound)
+# Normalizacion de variables numericas (nkill, nwound)
 preproc <- preProcess(selected_data %>% select(nkill, nwound), method = c("center", "scale"))
 normalized_data <- predict(preproc, selected_data %>% select(nkill, nwound))
 
-# Combinar datos normalizados con datos no numéricos
+# Combinar datos normalizados con datos no numericos
 final_data <- bind_cols(selected_data %>% select(-nkill, -nwound), normalized_data)
 
 # Guardar los datos procesados
