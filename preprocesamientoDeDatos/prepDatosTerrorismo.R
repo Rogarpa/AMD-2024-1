8bf2e2faa6e3dd94305cfa3f7eeb432e65fb143b
Luis Antonio Bello González <80022559+LuisBellocs@users.noreply.github.com>
2023-12-03 04:35:53 -0600

    Update prepDatosTerrorismo.R

--- preprocesamientoDeDatos/prepDatosTerrorismo.R
+++ preprocesamientoDeDatos/prepDatosTerrorismo.R
@@ -45,7 +45,7 @@ seleccion_de_atributos$attacktype2[is.na(seleccion_de_atributos$attacktype2)] <-
 moda_attacktype3 <- as.numeric(names(sort(table(seleccion_de_atributos$attacktype3), decreasing = TRUE)[1]))
 seleccion_de_atributos$attacktype3[is.na(seleccion_de_atributos$attacktype3)] <- moda_attacktype3
 
-# Reemplazar las cadenas vacías con a en las columnas "provstate", "city", "attacktype2" y "attacktype3"
+# Reemplazar las cadenas vacías en las columnas "provstate", "city", "attacktype2" y "attacktype3"
 seleccion_de_atributos$provstate[seleccion_de_atributos$provstate == ""] <- "Desconocido"
 seleccion_de_atributos$city[seleccion_de_atributos$city == ""] <- "Desconocido"
 seleccion_de_atributos$attacktype2_txt[seleccion_de_atributos$attacktype2_txt == ""] <- "Armed Assault"
@@ -53,32 +53,72 @@ seleccion_de_atributos$attacktype3_txt[seleccion_de_atributos$attacktype3_txt ==
 
 ####### VALORES ATÍPICOS #######
 
+seleccion_de_atributos2 <- seleccion_de_atributos
+valores_perdidos_data <- valores_perdidos(seleccion_de_atributos2)
+
+attacktype_herarchy = c("Assassination", "Hijacking", "Hostage Taking (Kidnapping)", "Hostage Taking (Barricade Incident)"
+                        , "Bombing/Explosion", "Armed Assault", "Unarmed Assault", "Facility/Infrastructure Attack", "Unknown")
+
+seleccion_de_atributos2$attacktype1_txt = as.numeric(factor(seleccion_de_atributos2$attacktype1_txt, levels=attacktype_herarchy))
+seleccion_de_atributos2$attacktype2_txt = as.numeric(factor(seleccion_de_atributos2$attacktype2_txt, levels=attacktype_herarchy))
+seleccion_de_atributos2$attacktype3_txt = as.numeric(factor(seleccion_de_atributos2$attacktype3_txt, levels=attacktype_herarchy))
+
 par(mfrow = c(4, 4), mar = c(2, 2, 2, 2))
-boxplot(seleccion_de_atributos$country, ylab = "country")
-boxplot(seleccion_de_atributos$latitude, ylab = "latitude")
-boxplot(seleccion_de_atributos$longitude, ylab = "longitude")
-boxplot(seleccion_de_atributos$specificity, ylab = "specificity")
-boxplot(seleccion_de_atributos$attacktype1, ylab = "attacktype1")
-boxplot(seleccion_de_atributos$attacktype2, ylab = "attacktype2")
-boxplot(seleccion_de_atributos$attacktype3, ylab = "attacktype3")
+boxplot(seleccion_de_atributos2$country, ylab = "country")
+boxplot(seleccion_de_atributos2$latitude, ylab = "latitude")
+boxplot(seleccion_de_atributos2$longitude, ylab = "longitude")
+boxplot(seleccion_de_atributos2$specificity, ylab = "specificity")
+boxplot(seleccion_de_atributos2$attacktype1_txt, ylab = "attacktype1_txt")
+boxplot(seleccion_de_atributos2$attacktype2_txt, ylab = "attacktype2_txt")
+boxplot(seleccion_de_atributos2$attacktype3_txt, ylab = "attacktype3_txt")
 
+# Extracción de valores atípicos basado en criterios IQR 
+outliers_country <- boxplot.stats(seleccion_de_atributos2$country)$out
+outliers_latitude <- boxplot.stats(seleccion_de_atributos2$latitude)$out
+outliers_longitude <- boxplot.stats(seleccion_de_atributos2$longitude)$out
+outliers_specificity <- boxplot.stats(seleccion_de_atributos2$specificity)$out
+outliers_attacktype1_txt <- boxplot.stats(seleccion_de_atributos2$attacktype1_txt)$out
+outliers_attacktype2_txt <- boxplot.stats(seleccion_de_atributos2$attacktype2_txt)$out
+outliers_attacktype3_txt <- boxplot.stats(seleccion_de_atributos2$attacktype3_txt)$out
 
-# Extract potential outliers based on IQR criterion
-outliers_country <- boxplot.stats(seleccion_de_atributos$country)$out
-outliers_latitude <- boxplot.stats(seleccion_de_atributos$latitude)$out
-outliers_longitude <- boxplot.stats(seleccion_de_atributos$longitude)$out
-outliers_specificity <- boxplot.stats(seleccion_de_atributos$specificity)$out
-outliers_attacktype1 <- boxplot.stats(seleccion_de_atributos$attacktype1)$out
-outliers_attacktype2 <- boxplot.stats(seleccion_de_atributos$attacktype2)$out
-outliers_attacktype3 <- boxplot.stats(seleccion_de_atributos$attacktype3)$out
+# Identificación de numero de indices de columnas con valores atípicos
+outlier_indices_country <- which(seleccion_de_atributos2$country %in% outliers_country)
+outlier_indices_latitude <- which(seleccion_de_atributos2$latitude %in% outliers_latitude)
+outlier_indices_longitude <- which(seleccion_de_atributos2$longitude %in% outliers_longitude)
+outlier_indices_specificity <- which(seleccion_de_atributos2$specificity %in% outliers_specificity)
+outlier_indices_attacktype1_txt <- which(seleccion_de_atributos2$attacktype1_txt %in% outliers_attacktype1_txt)
+outlier_indices_attacktype2_txt <- which(seleccion_de_atributos2$attacktype2_txt %in% outliers_attacktype2_txt)
+outlier_indices_attacktype3_txt <- which(seleccion_de_atributos2$attacktype3_txt %in% outliers_attacktype3_txt)
 
-# Identify row numbers corresponding to outliers
-outlier_indices_country <- which(seleccion_de_atributos$country %in% outliers_country)
-outlier_indices_latitude <- which(seleccion_de_atributos$longitude %in% outliers_latitude)
-outlier_indices_longitude <- which(seleccion_de_atributos$longitude %in% outliers_longitude)
-outlier_indices_attacktype1 <- which(seleccion_de_atributos$longitude %in% outliers_attacktype1)
+valores_atipicos_country <- seleccion_de_atributos2[outlier_indices_country, ]$country
+moda_country <- as.numeric(names(sort(table(seleccion_de_atributos2$country), decreasing = TRUE)[1]))
+seleccion_de_atributos2$country[seleccion_de_atributos2$country %in% valores_atipicos_country] <- moda_country
 
-# Print rows corresponding to outliers
-seleccion_de_atributos[outlier_indices_country, ]
-seleccion_de_atributos[outlier_indices_latitude, ]
-seleccion_de_atributos[outlier_indices_attacktype1, ]
+valores_atipicos_latitude <- seleccion_de_atributos2[outlier_indices_latitude, ]$latitude
+seleccion_de_atributos2$latitude[seleccion_de_atributos2$latitude %in% valores_atipicos_latitude] <- media_latitude
+
+valores_atipicos_longitude <- seleccion_de_atributos2[outlier_indices_longitude, ]$longitude
+seleccion_de_atributos2$longitude[seleccion_de_atributos2$longitude %in% valores_atipicos_longitude] <- media_longitude
+
+valores_atipicos_specificity <- seleccion_de_atributos2[outlier_indices_specificity, ]$specificity
+moda_specificity <- as.numeric(names(sort(table(seleccion_de_atributos2$specificity), decreasing = TRUE)[1]))
+seleccion_de_atributos2$specificity[seleccion_de_atributos2$specificity %in% valores_atipicos_specificity] <- moda_specificity
+
+valores_atipicos_attacktype1_txt <- seleccion_de_atributos2[outlier_indices_attacktype1_txt, ]$attacktype1_txt
+moda_attacktype1_txt <- as.numeric(names(sort(table(seleccion_de_atributos2$attacktype1_txt), decreasing = TRUE)[1]))
+seleccion_de_atributos2$attacktype1_txt[seleccion_de_atributos2$attacktype1_txt %in% valores_atipicos_attacktype1_txt] <- moda_attacktype1_txt
+
+valores_atipicos_attacktype2_txt <- seleccion_de_atributos2[outlier_indices_attacktype2_txt, ]$attacktype2_txt
+moda_attacktype2_txt <- as.numeric(names(sort(table(seleccion_de_atributos2$attacktype2_txt), decreasing = TRUE)[1]))
+seleccion_de_atributos2$attacktype2_txt[seleccion_de_atributos2$attacktype2_txt %in% valores_atipicos_attacktype2_txt] <- moda_attacktype2_txt
+
+valores_atipicos_attacktype3_txt <- seleccion_de_atributos2[outlier_indices_attacktype3_txt, ]$attacktype3_txt
+moda_attacktype3_txt <- as.numeric(names(sort(table(seleccion_de_atributos2$attacktype3_txt), decreasing = TRUE)[1]))
+seleccion_de_atributos2$attacktype3_txt[seleccion_de_atributos2$attacktype3_txt %in% valores_atipicos_attacktype3_txt] <- moda_attacktype3_txt
+
+seleccion_de_atributos3 <- seleccion_de_atributos2[, c(
+  "eventid", "iyear", "imonth", "iday",
+  "country", "region", "latitude", "longitude",
+  "specificity", "vicinity", "attacktype1_txt", "attacktype2_txt",
+  "attacktype3_txt", "extended", "success", "suicide"
+)]
